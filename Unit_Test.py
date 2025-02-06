1325a8a07fc9444d46c6f540ba0e4b1bab493d4c
MKolviner <164807738+MKolviner@users.noreply.github.com>
2025-02-06 13:04:06 +0300

    Add files via upload
    v2

--- Unit_Test.py
+++ Unit_Test.py
@@ -1,14 +1,29 @@
 from unittest import TestCase
-from main import unit_test
+from main import PasswordValidator
 
 
 class TestPasswordValidator(TestCase):
 
-    def test_password_validator(self):
-        self.assertNotEqual(unit_test("123"), "Пароль слишком короткий!")
+    def test_password_validator_0(self):
+        pas = PasswordValidator()
+        actual = pas.check_password("123")
+        expected = "Пароль слишком короткий!"
+        self.assertEqual(actual, expected)
 
     def test_password_validator_2(self):
-        self.assertNotEqual(unit_test("qwerty"), "Пароль должен содержать цифры!")
+        pas = PasswordValidator()
+        actual = pas.check_password("qwerty")
+        expected = "Пароль должен содержать цифры!"
+        self.assertEqual(actual, expected)
 
     def test_password_validator_3(self):
-        self.assertNotEqual(unit_test("qwerty123"), "Пароль должен содержать спецсимволы!")
+        pas = PasswordValidator()
+        actual = pas.check_password("qwerty123")
+        expected = "Пароль должен содержать спецсимволы!"
+        self.assertEqual(actual, expected)
+
+    def test_password_validator_4(self):
+        pas = PasswordValidator()
+        actual = pas.check_password("qwerty1@2#3")
+        expected = "Пароль соответствует требованиям"
+        self.assertEqual(actual, expected)
